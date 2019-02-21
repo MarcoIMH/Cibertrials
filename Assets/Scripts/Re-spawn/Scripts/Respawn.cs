508fdc31a40119600dde959b4d632f7005c9ee61
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2019-02-21 18:07:04 +0100

    Reaparicion, puntos de spawn y zona de muerte
    Ademas de los scripts he puesto el prefab de las zonas de checkpoint.
    Uno te pone una nueva posicion para respaunear, otro te hace "respaunear"y el ultimo simplemente activa la reaparicion cuando entramos en contacto con el trigger al que está asociado

---
+++ Assets/Scripts/Re-spawn/Scripts/Respawn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    public ChangeSpawn changeSpawn;             //Referencia a uno de los triggers para tener el spawnpoint
+	
+    public void ChangePos()
+    {
+        transform.position = changeSpawn.spawnpoint.position;                   //Posiciona al jugador en la posicion del objeto vacio SpawnPoint
+    }
+}
