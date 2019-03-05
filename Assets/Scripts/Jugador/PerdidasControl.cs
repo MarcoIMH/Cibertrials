458d2cfbc7b8b24f41b9d4b47ee1afe75ac098e9
nicoFdez <nicolafe@ucm.es>
2019-03-06 00:40:51 +0100

    added coments
    añadidos comentarios a los scripts de perdidas de control y Cubo de Hielo

--- Assets/Scripts/Jugador/PerdidasControl.cs
+++ Assets/Scripts/Jugador/PerdidasControl.cs
@@ -4,51 +4,68 @@ using UnityEngine;
 
 public class PerdidasControl : MonoBehaviour {
 
+    //segundos que dura la modificacion de velocidad
     public float  segundosModificaVelocidad;
+
+    //acceso al prefab del cubo de hielo para instanciarlo
     public CubitoHielo cuboHielo;
 
+    //variable que indica si el jugador esta congelado
+    //se usa para activar o desactivar los controles del jugador
     bool congelado = false;
 
     ControladorJugador controles;
 
     private void Start()
     {
+        //acceso al controlador jugador para llamar al switch
         if(this.gameObject.GetComponent<ControladorJugador>()!=null)
         controles = this.gameObject.GetComponent<ControladorJugador>();
 
-        AplicarCuboDeHielo();
+        //AplicarCuboDeHielo();
     }
 
+    //cambia el estado de los controles a true
     public void ActivaControles()
     {
         controles.CambiosPerdidaControl(PerdidaControles.stun, 1, true);
     }
 
+    //cambia el estado de los controles a false
     public void DesactivaControles(int segundos)
     {
+        //desactiva los controles
         controles.CambiosPerdidaControl(PerdidaControles.stun, 1, false);
+        //los vuelve a activar pasados "segundos" segundos
         Invoke("ActivaControles", segundos);
     }
 
+    //modifica la velocidad
     public void ActivaModificaVelocidad( float velocidadModificada)
     {
+        //modifica la velocidad
         controles.CambiosPerdidaControl(PerdidaControles.ralentizar, velocidadModificada, true);
+        //la devuelve a su valor normal pasados "segundosModificaVelocidad" segundos
         Invoke("DesactivaModificaVelocidad", segundosModificaVelocidad);
     }
 
+    //restaura la velocidad del jugador
     public void DesactivaModificaVelocidad()
     {
-        controles.RestauraVelocidad();
-        
+        controles.RestauraVelocidad(); 
     }
 
+    //activa el cubode hielo y congela al jugador poniendo el estado de los controles a false
     public void AplicarCuboDeHielo()
     {
         congelado = true;
+        //pone el estado de los controles a false
         controles.CambiosPoderes(Poderes.cubito, congelado);
+        //instancia el cubo de hielo entrando su script en ejecucion 
         CubitoHielo newCuboHielo = Instantiate<CubitoHielo>(cuboHielo, transform);
     }
 
+    //reactiva los controles
     public void DesactivarCuboDeHielo()
     {
         congelado = false;
--- Assets/Scripts/Poderes/CubitoHielo.cs
+++ Assets/Scripts/Poderes/CubitoHielo.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class CubitoHielo : MonoBehaviour {
 
+    //golpes necesarios para romper el bloque de hielo
     public int GolpesParaRomper;
 
+    //numero de golpes que ya ha recibido el cubo de hielo
     int GolpesRecibidos;
 
+    //variables de acceso a otros componentes
     RomperParedes picar;
     PerdidasControl pc;
 
 	// Use this for initialization
 	void Start ()
     {
-        GolpesRecibidos = 0;
+        GolpesRecibidos = 0; // al principio aun no ha recibido ningun golpe
 
+        //acceso a Romper Paredes para usar la misma tecla
         if (this.gameObject.GetComponentInParent<RomperParedes>() != null)
         {
             picar = this.gameObject.GetComponentInParent<RomperParedes>();
         }
 
+        //acceso a PerdidasControl para llamar a activar/desactivar el cubo de hielo
         if (this.gameObject.GetComponentInParent<PerdidasControl>() != null)
         {
             pc = this.gameObject.GetComponentInParent<PerdidasControl>();
@@ -30,10 +35,13 @@ public class CubitoHielo : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //si se pulsa la tecla de picar el cubo recibe un golpe
         if (Input.GetKeyDown(KeyCode.G)/*picar.tecla*/)
         {
             GolpesRecibidos++;
         }
+
+        //si se alcanza el numero de bloques necesarios se desactiva el cubo y se destruye
         if (GolpesRecibidos >= GolpesParaRomper)
         {
             pc.DesactivarCuboDeHielo();
