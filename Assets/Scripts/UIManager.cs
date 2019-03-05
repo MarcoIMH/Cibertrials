8d843cf42071c669b5d181fd7f83cc196d9e5d7c
Sandra <sandrumcr@gmail.com>
2019-03-05 19:27:50 +0100

    Añadido UI
    Antes no adjunté la UI, ahora queda adjuntada

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Image[] gemaJugador = new Image[6]; //0...2 player1, de 3...5 player2
+    int gemasMax;
+    void Start () {
+
+        GameManager.instance.SetUI(this);
+        gemasMax = gemaJugador.Length / 2;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ActualizaGema(int gema, Player jugador)
+    {
+ 
+        if (gema == 0)
+        {
+            int min, max;
+            if(jugador == Player.jugador1)
+            {
+                min = 0;
+                max = gemasMax;
+            }
+            else
+            {
+                min = gemasMax;
+                max = gemaJugador.Length;
+            }
+            for(int i = min; i < max; i++)
+            {
+                gemaJugador[i].gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (jugador == Player.jugador1) gemaJugador[gema-1].gameObject.SetActive(true);
+            else gemaJugador[gema + gemasMax-1].gameObject.SetActive(true);
+        }
+    }
+
+}
