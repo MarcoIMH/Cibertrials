dafa1d7087b4abc6ab48aa3cf58f58d9afbcd728
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2019-03-06 14:47:06 +0100

    Prefab de la tuberia corroida (Amenazas de mapa)
    He subido el prefab de la tuberia con los sprites que requiere y el código que lo hace funcionar todo. Se puede modificar desde el editor la velocidad a la que cae, cuanto tarda en destruirse y cuanto en volver a aparecer.

---
+++ Assets/Scripts/Mapa/Tuberia.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuberia : MonoBehaviour {
+
+    /// <summary>
+    /// Estas plataformas es posible colocarlas en modo espejo poniendo la escala del padre a su valor pero en negativo
+    /// Es posible que se pueda quitar la comprobacion por tags ya que puede que no se pongan enemigos en este tipo de plataformas
+    /// </summary>
+
+
+
+    public float tiempoRotura, tiempoReparacion, velCaida;            //Tiempos de casting
+
+    private bool caida = false;             
+    private Vector3 posicionIni;
+
+    void Start()
+    {
+        posicionIni = transform.position;           //Guardamos la posicioninicial de la tuberia
+        
+    }
+
+    void Update ()
+    {
+        if (caida == true)          //El tubo se desprende y comienza la caida libre rotando un poco
+        {
+            transform.Rotate(0f, 0f, -3f * Time.deltaTime);
+            transform.Translate(Vector2.down * Time.deltaTime * velCaida);
+        }
+    }
+
+    /// <summary>
+    /// Este metodo lo que hace es ver como se ha producido la colision(debe de ser desde arriba) y quien ha colisionado(si no es uno de los jugadores no hace nada)
+    /// Tras esto pone caida a true para que se empiece a ejecutar e invoca a dos métodos: uno que desactiva la tuberia y otro que la reactivará en la posicion que tenia originalmente
+    /// </summary>
+    /// <param name="other"></param>
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.contacts[0].normal == Vector2.down && other.gameObject.CompareTag("jugador1") || other.gameObject.CompareTag("jugador2"))
+        {
+            caida = true;
+            Invoke("RomperTubo", tiempoRotura);             //Tiempo hasta que se ejecuta el metodo RomperTubo
+            Invoke("RepararTubo", tiempoReparacion + tiempoRotura);             //El tiempo de casting será la suma de los 2 ya que se activan a la vez y así la diferencia es el tiempoReparacion original
+        }
+    }
+	
+	void RomperTubo()       //Falsea caida y desactiva la tuberia
+    {
+        caida = false;
+        this.gameObject.SetActive(false);
+    }
+
+    void RepararTubo()      //Recoloca , orienta la tuberia y la reactiva
+    {
+        transform.position = posicionIni;               //La posicion pasa a ser la inicial
+        transform.rotation = Quaternion.identity;       //Ponemos la rotacion a (0,0,0)
+        this.gameObject.SetActive(true);                //Se reactiva el gameObject
+    }
+}
