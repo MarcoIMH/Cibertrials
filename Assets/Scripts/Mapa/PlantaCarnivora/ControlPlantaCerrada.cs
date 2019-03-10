919f7a170b9d64ab2f904e3db0d8ef1b3bea1461
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2019-03-10 11:18:57 +0100

    Prefab, scripts y imagenes provisionales de la planta carnívora
    La planta funciona de la siguiente manera:
    Cuando algo la toca por la parte de arriba esta ejecuta dos metodos cado uno con un retardo que son para poner el sprite de la planta cerrada con su trigger zona de muerte y otro que vuelve a poner la planta en modo abierto para que se vuelva a repetir el ciclo.
    
    La base de la planta es el padre de los dos capullos de la planta y por lo tanto podemos obtener los componentes que tienen sus hijos.
    
    Ha sido probado poniendo gran cantidad de plantas y cada uno funciona de forma independiente a las otras no dando ningún error.

---
+++ Assets/Scripts/Mapa/PlantaCarnivora/ControlPlantaCerrada.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlantaCerrada : MonoBehaviour {
+    /// <summary>
+    /// Este componente asociado al capullo cerrado de la planta controla si el objeto está activo o no segun lo dicte el
+    /// script PlantaCarnivora
+    /// </summary>
+	public void ActivarObjeto()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void DesactivarObjeto()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Mapa/PlantaCarnivora/PlantaCarnivora.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantaCarnivora : MonoBehaviour {
+
+    public float retardo,reapertura;
+    private ControlPlantaAbierta CapulloA;
+    private ControlPlantaCerrada CapulloB;
+
+
+    
+    void Start () {
+       CapulloA = GetComponentInChildren<ControlPlantaAbierta>();       //Componente del sprite de la con el capullo abierto
+       CapulloB = GetComponentInChildren<ControlPlantaCerrada>();      //Componente del sprite de la con el capullo cerrado
+
+        CapulloB.DesactivarObjeto();        //Como para poder localizarlo tiene que estar activo al encontrarlo lo desactivamos
+    }
+	
+	
+	void Update () {
+		
+	}
+    /// <summary>
+    /// Al entrar en colision con la base de la plata esta ejecuta dos metodos, uno que hará que la planta se cierre y otro que hará
+    /// que despues se habra de nuevo. Ambos tiempos son configurables desde el editor
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.contacts[0].normal == Vector2.down)       //Si el golpe es por arriba si que permite la ejecución
+        {
+            Invoke("AtaquePlanta", retardo);
+            Invoke("DeshacerAtaquePlanta", retardo + reapertura);
+            //Como ambos metodos se ejecutan a la misma vez hay sumarle al retardo del segundo el del primero para que se mantenga la diferencias
+        }
+       
+    }
+    //Pone uno de los sprites desactivados y activa el otro
+    void AtaquePlanta()
+    {
+        CapulloA.DesactivarObjeto();
+        CapulloB.ActivarObjeto();
+    }
+
+    //Deshace el metodo de ataque planta, vuele a abrir la flor
+    void DeshacerAtaquePlanta()
+    {
+        CapulloA.ActivarObjeto();
+        CapulloB.DesactivarObjeto();
+    }
+}
