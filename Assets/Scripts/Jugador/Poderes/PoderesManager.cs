3622c44d15c4e5eeabeeb3b1de8179a7d71a0419
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2019-03-12 19:46:21 +0100

    UPDATED: *.cs para hito
    Actualizado los scripts para el hito

--- Assets/Scripts/Jugador/Poderes/PoderesManager.cs
+++ Assets/Scripts/Jugador/Poderes/PoderesManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class PoderesManager : MonoBehaviour {
     
-    public GameObject jugadorContrario, muro, neblina, cuboHielo;                                                                           //GO para gestionar el jugador contrario. Prefabs de muro y neblina para su instancia.
+    public GameObject jugadorContrario, muro, neblina, cuboHielo, neb1,neb2;                                                                           //GO para gestionar el jugador contrario. Prefabs de muro y neblina para su instancia.
     public Player jugador;
     public KeyCode teclaPoder;
     public int gemasMax;
-    public float segundosInversionControles;
+    public float segundosInversionControles, tiempoNeblina;
     
 
     PerdidasControl pc;
@@ -22,7 +22,8 @@ public class PoderesManager : MonoBehaviour {
         public float y;
     }
     Coordenadas[] coordsPoderesMapa;                                                                                             //Vector para almacenar todas las coordenadas de poderes del mapa que se cargue en escena
-
+                                                                                         //Vector para almacenar todas las coordenadas de poderes del mapa que se cargue en escena
+                                                                                         //CAMBIAR AL MÉTODO NORMAL DE INSTANCIA DE PODERES (NEBLINA/MURO)
     Poderes[] poder = new Poderes[4]; //array de poderes
     Poderes poderUsar;
     int gemas;
@@ -45,6 +46,7 @@ public class PoderesManager : MonoBehaviour {
             controlesJugadorContrario = jugadorContrario.gameObject.GetComponent<ControladorJugador>();
         }
 
+       
         Invoke("ConfiguraCoordenadasPoderes", 1f);                                                                               //Invocamos la carga de coordenadas del mapa un segundo después  como seguridad 
     }                                                                                                                            //para que de tiempo a todo a situarse en su lugar
 
@@ -65,7 +67,8 @@ public class PoderesManager : MonoBehaviour {
                     AplicarCuboDeHielo();
                     break;
                 case Poderes.muro:
-                    ActivaMuro();
+                    //ActivaMuro();                                     //DEVOLVER EL MURO A SU LUGAR CUANDO SE PASE EL HITO
+                    ActivaNeblina();
                     break;
                 case Poderes.neblina:
                     ActivaNeblina();
@@ -94,87 +97,7 @@ public class PoderesManager : MonoBehaviour {
             return true;
         }
         else return false;             
-    }
-
-    void BuscaHabilidad()
-    {   
-        poderUsar = poder[Random.Range(0,4)];                                                                                    //Lo pongo en 3,4 para forzar que use neblina. Por defecto es 0 4. 
-                                                                                                                                 //Devolver a la normalidad cuando termine de hacer los poderes
-    }
-    
-    /// <summary>
-    /// Método para configurar las coordenadas de los poderes/mapa, solicita al GameManager el nombre de la escena y llama al método para cargar coordenadas de ese mapa
-    /// </summary>
-    void ConfiguraCoordenadasPoderes()
-    {
-        string mapa = GameManager.instance.NombreEscena();                                                                       //Preguntamos a GameManager por el nombre de la escena
-        LeeCordenadasPoderesDelMapa("Assets/Scripts/Mapa/MurosMapa/"+mapa+".txt");                                               //Cargamos el archivo correspondiente a esa escena respetando patrón de ruta.    
-    }
-
-    /// <summary>
-    /// Método para cargar las coordenadas de las habilidades muro/neblina asociados al mapa nombreMapa en el vector coordsMuros. EN CASO DE TENER DOS MAPAS HAY QUE CONFIGURARLO PARA QUE DISTINGA MUROS POR JUGADOR
-    /// </summary>
-    /// <param name="nombreMapa"></param>
-    void LeeCordenadasPoderesDelMapa(string nombreMapa)
-    {
-        StreamReader reader = new StreamReader(nombreMapa);                                                                       //Reader para leer el txt
-
-        if (reader != null)
-        {
-            int cuantos = int.Parse(reader.ReadLine());                                                                           //La primera línea de cada txt con las coordenadas del mapa es la cantidad de coordenadas que hay
-            coordsPoderesMapa = new Coordenadas[cuantos];                                                                         //Instanciamos el tamaño del vector de coordenadas con el número de coordenadas que hay
-
-            for (int x = 0; x < coordsPoderesMapa.Length; x++)                                                                    //Recorremos tantas líneas a partir de ahí como coordenadas haya
-            {
-                string linea = reader.ReadLine();                                                                                //En cada línea recogemos el texto. Esta trae coord x e y, separadas por un espacio
-                string[] coords = linea.Split(' ');                                                                              //Lo separamos por patrón de espacio
-                coordsPoderesMapa[x].x = float.Parse(coords[0]);                                                                 //Y asignamos cada valor a la correspondiente coordenada
-                coordsPoderesMapa[x].y = float.Parse(coords[1]);
-            }
-        }        
-        reader.Close();
-    }
-
-    /// <summary>
-    /// Método para activar el muro, checkea la posición del jugador contrario e instancia el muro en la siguiente posición permitida cuando el poder sea activado                       
-    /// </summary>
-    void ActivaMuro()
-    {        
-        int bandera = 0;                                                                                                          //Bandera para recorrer el array de coordenadas de poderes en mapa
-        CalculaPosicionPoder(ref bandera);
-        Instantiate(muro, new Vector3(coordsPoderesMapa[bandera].x, coordsPoderesMapa[bandera].y, 0), Quaternion.identity);       //Instanciamos el muro en la siguiente coordenada al jugador contrario
-    }
-
-    /// <summary>
-    /// Método para activar el muro, checkea la posición del jugador contrario e instancia la neblina en la siguiente posición permitida cuando el poder sea activado   
-    /// </summary>
-    void ActivaNeblina()
-    {
-        int bandera = 0;                                                                                                          //Bandera para recorrer el array de coordenadas de poderes en mapa
-        CalculaPosicionPoder(ref bandera);
-        float ancho = coordsPoderesMapa[bandera + 1].x - coordsPoderesMapa[bandera].x;                                            //Calculamos el ancho que tendrá la Neblina, dependerá de la distancia entre dos coordenadas sucesivas
-        var niebla = Instantiate(neblina, new Vector3(coordsPoderesMapa[bandera].x, coordsPoderesMapa[bandera].y, 0), Quaternion.identity) as GameObject;      //Instanciamos la neblina en la siguiente coordenada al jugador contrario
-        niebla.transform.localScale = new Vector3(ancho,40f,0f);                                                                  //Configuramos su tamaño
-        niebla.layer = LayerMask.NameToLayer("Neblina");                                                                          //Acomodamos la Neblina en su layer
-    }
-
-    /// <summary>
-    /// Método para calcular la posición en la que se lanzará el poder Muro o Neblina. Al salir del método bandera apuntará a la coordenada de lanzamiento del poder
-    /// </summary>
-    /// <param name="bandera"></param>
-    void CalculaPosicionPoder(ref int bandera)
-    {
-        Transform transformJC;
-        if (jugadorContrario.GetComponent<Transform>() != null)
-        {
-            transformJC = jugadorContrario.GetComponent<Transform>();                                                            //Si el JC está activo guarda su transform para gestionar conocer su posición
-
-            while (transformJC.position.x >= coordsPoderesMapa[bandera].x && bandera+1<coordsPoderesMapa.Length)                 //Mientras que la coordenada del jugador contrario sea menor que la del muro en esa posición sigue avanzando
-            {
-                bandera++;
-            }
-        }
-    }
+    } 
 
     /// <summary>
     /// //activa el cubode hielo y congela al jugador poniendo el estado de los controles a false
@@ -215,5 +138,111 @@ public class PoderesManager : MonoBehaviour {
     public void DesactivaInvierteControles()
     {
         controlesJugadorContrario.CambiosPoderes(Poderes.inversionControles, false);
+    }    
+
+
+    void BuscaHabilidad()
+    {
+        poderUsar = poder[Random.Range(0,3)];                                                                                    //Lo pongo en 3,4 para forzar que use neblina. Por defecto es 0 4. 
+                                                                                                                                 //Devolver a la normalidad cuando termine de hacer los poderes
+    }
+
+    /// <summary>
+    /// Método para configurar las coordenadas de los poderes/mapa, solicita al GameManager el nombre de la escena y llama al método para cargar coordenadas de ese mapa
+    /// </summary>
+    void ConfiguraCoordenadasPoderes()
+    {
+        string mapa;
+        //string mapa = GameManager.instance.NombreEscena();                                                                       //Preguntamos a GameManager por el nombre de la escena
+        if (jugador == Player.jugador1)
+            mapa = "J2";
+        else
+            mapa = "J1";
+        LeeCordenadasPoderesDelMapa("Assets/Scripts/Mapa/MurosMapa/" + mapa + ".txt");                                               //Cargamos el archivo correspondiente a esa escena respetando patrón de ruta.    
+    }
+
+    /// <summary>
+    /// Método para cargar las coordenadas de las habilidades muro/neblina asociados al mapa nombreMapa en el vector coordsMuros. EN CASO DE TENER DOS MAPAS HAY QUE CONFIGURARLO PARA QUE DISTINGA MUROS POR JUGADOR
+    /// </summary>
+    /// <param name="nombreMapa"></param>
+    void LeeCordenadasPoderesDelMapa(string nombreMapa)
+    {        StreamReader reader = new StreamReader(nombreMapa);                                                                       //Reader para leer el txt
+
+       
+            if (reader != null)
+            {
+                int cuantos = int.Parse(reader.ReadLine());                                                                           //La primera línea de cada txt con las coordenadas del mapa es la cantidad de coordenadas que hay
+                coordsPoderesMapa = new Coordenadas[cuantos];                                                                         //Instanciamos el tamaño del vector de coordenadas con el número de coordenadas que hay
+
+                for (int x = 0; x < coordsPoderesMapa.Length; x++)                                                                    //Recorremos tantas líneas a partir de ahí como coordenadas haya
+                {
+                    string linea = reader.ReadLine();                                                                                //En cada línea recogemos el texto. Esta trae coord x e y, separadas por un espacio
+                    string[] coords = linea.Split(' ');                                                                              //Lo separamos por patrón de espacio
+                    coordsPoderesMapa[x].x = float.Parse(coords[0]);                                                                 //Y asignamos cada valor a la correspondiente coordenada
+                    coordsPoderesMapa[x].y = float.Parse(coords[1]);
+                }
+            }
+         
+        reader.Close();
+    }
+
+    /// <summary>
+    /// Método para activar el muro, checkea la posición del jugador contrario e instancia el muro en la siguiente posición permitida cuando el poder sea activado                       
+    /// </summary>
+    void ActivaMuro()
+    {
+        int bandera = 0;                                                                                                          //Bandera para recorrer el array de coordenadas de poderes en mapa
+        CalculaPosicionPoder(ref bandera);
+        Instantiate(muro, new Vector3(coordsPoderesMapa[bandera].x, coordsPoderesMapa[bandera].y, 0), Quaternion.identity);       //Instanciamos el muro en la siguiente coordenada al jugador contrario
+    }
+
+    /// <summary>
+    /// Método para activar el muro, checkea la posición del jugador contrario e instancia la neblina en la siguiente posición permitida cuando el poder sea activado   
+    /// </summary>
+    void ActivaNeblina()
+    {
+        //MÉTODO 1
+        /*
+        int bandera = 0;                                                                                                          //Bandera para recorrer el array de coordenadas de poderes en mapa
+        CalculaPosicionPoder(ref bandera);
+        float ancho = coordsPoderesMapa[bandera + 1].x - coordsPoderesMapa[bandera].x;                                            //Calculamos el ancho que tendrá la Neblina, dependerá de la distancia entre dos coordenadas sucesivas
+        var niebla = Instantiate(neblina, new Vector3(coordsPoderesMapa[bandera].x, coordsPoderesMapa[bandera].y, 0), Quaternion.identity) as GameObject;      //Instanciamos la neblina en la siguiente coordenada al jugador contrario
+        niebla.transform.localScale = new Vector3(ancho, 40f, 0f);                                                                  //Configuramos su tamaño
+        niebla.layer = LayerMask.NameToLayer("Neblina");                                                                          //Acomodamos la Neblina en su layer
+        */
+
+        //MÉTODO 2 -> Se cambia temporalmente hasta tener los mapas definitivos en los que poder asignar coordenadas "reales" en los txt
+        if (jugador == Player.jugador1)
+            neb2.active = true;
+        else
+            neb1.active = true;
+
+        Invoke("DesactivaNeblina", tiempoNeblina);
+    }
+
+    void DesactivaNeblina()
+    {
+        if (jugador == Player.jugador1)
+            neb2.active = false;
+        else
+            neb1.active = false;
+    }
+
+    /// <summary>
+    /// Método para calcular la posición en la que se lanzará el poder Muro o Neblina. Al salir del método bandera apuntará a la coordenada de lanzamiento del poder
+    /// </summary>
+    /// <param name="bandera"></param>
+    void CalculaPosicionPoder(ref int bandera)
+    {
+        Transform transformJC;
+        if (jugadorContrario.GetComponent<Transform>() != null)
+        {
+            transformJC = jugadorContrario.GetComponent<Transform>();                                                            //Si el JC está activo guarda su transform para gestionar conocer su posición
+
+            while (transformJC.position.x >= coordsPoderesMapa[bandera].x && bandera + 1 < coordsPoderesMapa.Length)                 //Mientras que la coordenada del jugador contrario sea menor que la del muro en esa posición sigue avanzando
+            {
+                bandera++;
+            }
+        }
     }
 }
