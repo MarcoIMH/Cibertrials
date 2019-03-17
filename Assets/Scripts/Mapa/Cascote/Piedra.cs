692992dce7da839120dc76d0528c32861a78baa8
nicoFdez <nicolafe@ucm.es>
2019-03-17 15:24:13 +0100

    Añadida la amenaza de los cascotes
    he añdido los correspondientes scripts ZonaCascote y piedra, el prefab cascote y el sprite piedra

---
+++ Assets/Scripts/Mapa/Cascote/Piedra.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piedra : MonoBehaviour {
+
+    public float vCaida;
+    public float tDestruccion;
+
+    bool cae;
+    Vector2 mov;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mov = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(mov * Time.deltaTime);
+	}
+
+    public void IniciaMov()
+    {
+        mov = new Vector2(0, -vCaida);
+        Destroy(GetComponentInParent<Transform>().gameObject, tDestruccion);
+    }
+}
---
+++ Assets/Scripts/Mapa/Cascote/ZonaCascote.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonaCascote : MonoBehaviour {
+ 
+    Piedra piedra;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if(GetComponentInChildren<Piedra>() != null)
+        {
+            piedra = GetComponentInChildren<Piedra>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)//cuando el jugador entra en la zona
+    {
+        piedra.IniciaMov();
+    }
+
+}
