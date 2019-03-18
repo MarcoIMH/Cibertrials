a0046bf1b9efdba940d083ee62d61f255552ae7a
nicoFdez <nicolafe@ucm.es>
2019-03-18 21:34:40 +0100

    añadidas las plataformas con movimiento
    He añadido el prefab (PlataformaMovimiento) con su correspondinte script (PlataformaMov)
    
    Además he modificado a la matriz de colisiones una nueva layer propia del trigger de la plataforma que se mueve para evitar bugs

---
+++ Assets/Scripts/Mapa/PlataformaMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMov : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {     
+        other.gameObject.transform.parent = this.gameObject.transform; //el objeto que se encuentra sobre la plataforma se vuelve hijo de esta
+                                                                       //actualmente solo el jugador (collision matrix) 
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {  
+        other.gameObject.transform.parent = null; // al saltar el objeto deja de estar encima y deja de ser hijo de la plataforma
+    }
+}
