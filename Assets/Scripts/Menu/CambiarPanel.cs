2e586b56ee71b4cc738fa42d03a3381416f41031
nicoFdez <nicolafe@ucm.es>
2019-04-01 21:02:58 +0200

    Añadido el menu principal
    añadida toda la funcionalidad del menu principal aunque aun con los paneles de configuracion y creaditos vacios

---
+++ Assets/Scripts/Menu/CambiarPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarPanel : MonoBehaviour {
+
+    public GameObject panelMenu,panelConfig,panelCreditos;
+
+    Animator panel;
+    string nombrePanel;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if(GetComponent<Animator>() != null)
+            panel = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Metodo para que desparezca el panel
+    /// </summary>
+    public void Desvanecerse()
+    {
+        panel.SetTrigger("fadeOut");
+    }
+
+    /// <summary>
+    /// Activa el trigger de la transicion dependiendo de el boton pulsado
+    /// </summary>
+    public void ActivaTrigger()
+    {
+        panel.SetTrigger(nombrePanel);
+
+        switch (nombrePanel)
+        {
+            case "menu":
+                panelMenu.gameObject.SetActive(true);
+                break;
+            case "configuracion":
+                panelConfig.gameObject.SetActive(true);
+                break;
+            case "creditos":
+                panelCreditos.gameObject.SetActive(true);
+                break;
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Segun el boton que se pulse el trigger será distinto
+    /// </summary>
+    /// <param name="sigPanel"></param>
+    public void EstablecePanel(string sigPanel)
+    {
+        nombrePanel = sigPanel;
+    }
+}
---
+++ Assets/Scripts/Menu/EscenasManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenasManager : MonoBehaviour
+{
+    public static EscenasManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Metodo para cargar las escenas
+    /// </summary>
+    /// <param name="escena"></param>
+    public void CargarEscena(string escena)
+    {
+        SceneManager.LoadScene(escena);
+    }
+
+    /// <summary>
+    /// Metodo al que se llama desde el boton salir para cerrar el juego
+    /// </summary>
+    public void CerrarJuego()
+    {
+        Application.Quit();
+    }
+}
