3063d7a81943494afa69cf40c57248c1ade3eda4
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2019-04-04 22:19:02 +0200

    ADDED: Menu configuracion controles inGame. UPDATED: ControlesManager.cs
    Añadida la sección del menú de configuración de controles para los jugadores.
    
    Actualizado ControlesManager para tener en cuenta la carga inicial y no realizar dobles asignaciones.

--- Assets/Scripts/ControlesManager.cs
+++ Assets/Scripts/ControlesManager.cs
@@ -8,11 +8,12 @@ public class ControlesManager : MonoBehaviour {
     public KeyCode teclaSaltar, teclaRodar, teclaPoder, teclaRomperParedes, teclaSaltoParedes, teclaIzqdaParedes, teclaDchaParedes, teclaMenu;
 
     Dictionary<string, KeyCode> controles = new Dictionary<string, KeyCode>();
+    bool cargaInicial = true;
 
     // Use this for initialization
     void Start () {
         CreaDiccionarioControles();
-        AsignaControlesJugador();
+        AsignaControlesInicialesJugador();
 	}
 	
 	// Update is called once per frame
@@ -32,7 +33,7 @@ public class ControlesManager : MonoBehaviour {
         controles.Add("Menu", teclaMenu);
     }
 
-    void AsignaControlesJugador()
+    void AsignaControlesInicialesJugador()
     {
         SetControlSaltar(teclaSaltar);
         SetControlRodar(teclaRodar);
@@ -45,56 +46,70 @@ public class ControlesManager : MonoBehaviour {
         SetControlIzqdaParedes(teclaIzqdaParedes);
         SetControlDchaParedes(teclaDchaParedes);
 
-        SetControlMenu(teclaMenu);        
+        SetControlMenu(teclaMenu);
 
-        GameManager.instance.SetControlesJugador(controles, tipoJugador);
+        InformaGameManager();
+        cargaInicial = false;
     }
 
     void SetControlSaltar(KeyCode nuevaTecla)
     {
         if (GetComponent<ControladorJugador>() != null)
             GetComponent<ControladorJugador>().SetTeclaSaltar(nuevaTecla);
+        if(!cargaInicial) controles["Saltar"] = nuevaTecla;
     }
 
     void SetControlRodar(KeyCode nuevaTecla)
     {
         if (GetComponent<ControladorJugador>() != null)
             GetComponent<ControladorJugador>().SetTeclaRodar(nuevaTecla);
+        if (!cargaInicial) controles["Rodar"] = nuevaTecla;
     }
 
     void SetControlPoder(KeyCode nuevaTecla)
     {
         if (GetComponent<PoderesManager>() != null)
             GetComponent<PoderesManager>().SetTeclaPoder(nuevaTecla);
+        if (!cargaInicial) controles["Poder"] = nuevaTecla;
     }
 
     void SetControlRomperParedes(KeyCode nuevaTecla)
     {
         if (GetComponent<RomperParedes>() != null)
             GetComponent<RomperParedes>().SetTeclaRomperParedes(nuevaTecla);
+        if (!cargaInicial) controles["RomperParedes"] = nuevaTecla;
     }
 
     void SetControlSaltoParedes(KeyCode nuevaTecla)
     {
         if (GetComponent<SaltoParedes>() != null)
             GetComponent<SaltoParedes>().SetTeclaSaltoParedes(nuevaTecla);
+        if (!cargaInicial) controles["SaltoParedes"] = nuevaTecla;
     }
 
     void SetControlIzqdaParedes(KeyCode nuevaTecla)
     {
         if (GetComponent<SaltoParedes>() != null)
             GetComponent<SaltoParedes>().SetTeclaIzqdaParedes(nuevaTecla);
+        if (!cargaInicial) controles["IzqdaParedes"] = nuevaTecla;
     }
 
     void SetControlDchaParedes(KeyCode nuevaTecla)
     {
         if (GetComponent<SaltoParedes>() != null)
             GetComponent<SaltoParedes>().SetTeclaDchaParedes(nuevaTecla);
+        if (!cargaInicial) controles["DchaParedes"] = nuevaTecla;
     }
 
     void SetControlMenu(KeyCode nuevaTecla)
     {
         GameManager.instance.SetTeclaMenu(nuevaTecla, tipoJugador);
+        if (!cargaInicial) controles["Menu"] = nuevaTecla;
+    }
+
+    void InformaGameManager()
+    {
+        GameManager.instance.SetControlesJugador(controles, tipoJugador);
     }
 
     public void CambiaControlJugador(string nombreControl, KeyCode nuevaTecla)
@@ -110,5 +125,6 @@ public class ControlesManager : MonoBehaviour {
             case "DchaParedes": SetControlDchaParedes(nuevaTecla); break;
             case "Menu": SetControlMenu(nuevaTecla); break;
         }
+        InformaGameManager();
     }
 }
