83de15d94e6eaa61540a4d4fac35e29a8769a7a8
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2019-05-08 03:52:26 +0200

    ADDED: Cheats - SiguienteRespawn. Cheats - UltimoRespawn
    Implementadas funcionalidades cheats para moverse entre los distintos respawns en el momento que se desee.

--- Assets/Scripts/Cheats/Cheats.cs
+++ Assets/Scripts/Cheats/Cheats.cs
@@ -7,8 +7,9 @@ public class Cheats : MonoBehaviour {
     public GameObject jugador;
     public Player tipoJugador;
 
-    float gravedadPorDefecto = 0;
 
+    Transform[] coordsRespawns;
+    float gravedadPorDefecto = 0;
     bool volar = false, invencibilidad = false, gravedadRecogida = false;
 
     // Use this for initialization
@@ -40,5 +41,31 @@ public class Cheats : MonoBehaviour {
         jugador.GetComponent<ControladorJugador>().SetVolar(volar);
         if (volar) jugador.GetComponent<Rigidbody2D>().gravityScale = 0.1f;
         else jugador.GetComponent<Rigidbody2D>().gravityScale = gravedadPorDefecto;
+    }
+
+    public void SiguienteRespawn()
+    {
+        coordsRespawns = GameManager.instance.GetCoordenadasPoderes();
+        int indice = 1;
+        while (indice + 2 < coordsRespawns.Length && jugador.transform.position.x >= coordsRespawns[indice].position.x) indice++;
+        if(indice+2 < coordsRespawns.Length)
+        {
+            Vector3 posicion;
+            if (tipoJugador == Player.jugador1)
+                posicion = new Vector3(coordsRespawns[indice].localPosition.x, coordsRespawns[indice].localPosition.y + coordsRespawns[indice].transform.localScale.y * 2, 0);
+            else posicion = new Vector3(coordsRespawns[indice].position.x, coordsRespawns[indice].position.y + coordsRespawns[indice].transform.localScale.y * 2, 0);
+            jugador.transform.position = posicion;
+        }            
+    }
+
+    public void UltimoRespawn()
+    {
+        coordsRespawns = GameManager.instance.GetCoordenadasPoderes();
+        Vector3 posicion;
+        int indice = coordsRespawns.Length-2;
+        if (tipoJugador == Player.jugador1)
+            posicion = new Vector3(coordsRespawns[indice].localPosition.x, coordsRespawns[indice].localPosition.y + coordsRespawns[indice].transform.localScale.y * 2, 0);
+        else posicion = new Vector3(coordsRespawns[indice].position.x, coordsRespawns[indice].position.y + coordsRespawns[indice].transform.localScale.y * 2, 0);
+        jugador.transform.position = posicion;
     }
 }
